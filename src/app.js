e9877a62b82113c0f12b7086f921ebd9d71f5715
Joe Garrett <joegarrett404@gmail.com>
2022-08-30 12:45:35 -0600

    react setup

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import { React, useState } from "react";
+import { ReactDOM } from "react-dom";
+import { NavLink, BrowserRouter, Route } from "react-router-dom";
+// Import react components
+import { } from './components/index';
+
+const App = () => {
+
+  // Set useStates
+
+  return (
+    <main>
+     
+      <nav>
+        {/* NavLinks */}
+      </nav>
+
+      {/* Routes and components */}
+    
+    </main>
+  )
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('app')
+)
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+// Use this file to export all components 
+// i.e. export {default as ______} from './_____'
