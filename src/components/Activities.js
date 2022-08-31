137e3e9919a6dfcfe9f85b799a84aa38516a136d
Joe Garrett <joegarrett404@gmail.com>
2022-08-30 21:30:08 -0600

    router setup

---
+++ src/components/Activities.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const Activities = () => {
+return <>
+<h1>Activities</h1>
+</>
+}
+
+export default Activities;
---
+++ src/components/PublicRoutines.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+const PublicRoutines = () => {
+    
+    return <>
+    <h1>PublicRoutines</h1>
+    </>
+    }
+    
+    export default PublicRoutines;
